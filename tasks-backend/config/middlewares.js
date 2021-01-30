f92817eab2ee25d40a7ac9dab93d773d6cd533f1
Breno Silvério <brenolimeira@gmail.com>
2021-01-30 13:07:06 -0300

    Version 1.9.8

--- tasks-backend/config/middlewares.js
+++ tasks-backend/config/middlewares.js
@@ -1,9 +1,12 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const express = require('express')
+const path = require('path')
 
 module.exports = app => {
     app.use(bodyParser.json())
     app.use(cors({
         origin: '*'
     }))
+    app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 }
